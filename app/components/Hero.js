29d2cd5a0982f3e628df800021a208171db15f33
bagus faize <bagusfaize@gmail.com>
2024-10-08 04:48:05 +0700

    change mock year

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -10,7 +10,7 @@ export default function Hero({
             <h5 className="font-semibold text-sm">THETICKET.COM</h5>
         </div>
         <div className="mt-5 mb-2">
-            <h1 className="mb-2 text-xl md:text-3xl font-bold">Book Your Ticket for Music Concert 2023</h1>
+            <h1 className="mb-2 text-xl md:text-3xl font-bold">Book Your Ticket for Music Concert 2024</h1>
             <p className="font-semibold">
                 {heading}
             </p>
